69fb9168312a8d43dd4577022f5c6a2d63804b51
tinywind <tinywind0@gmail.com>
2026-08-28 07:19:23 +0900

    chore(plugins): publish plugin manifest

--- .js/plugins/index.js
+++ .js/plugins/index.js
@@ -7,8 +7,20 @@ const contenttypefixture_1 = __importDefault(require("@plugins/dev/contenttypefi
 const projectgutenberg_1 = __importDefault(require("@plugins/english/projectgutenberg"));
 const standardebooks_1 = __importDefault(require("@plugins/english/standardebooks"));
 const aozorabunko_1 = __importDefault(require("@plugins/japanese/aozorabunko"));
+const ndlnextdigitallibrary_1 = __importDefault(require("@plugins/japanese/ndlnextdigitallibrary"));
 const githubdocs_1 = __importDefault(require("@plugins/multi/githubdocs"));
 const komga_1 = __importDefault(require("@plugins/multi/komga"));
 const oapen_1 = __importDefault(require("@plugins/multi/oapen"));
-const PLUGINS = [contenttypefixture_1.default, projectgutenberg_1.default, standardebooks_1.default, aozorabunko_1.default, githubdocs_1.default, komga_1.default, oapen_1.default];
+const peppercarrot_1 = __importDefault(require("@plugins/multi/peppercarrot"));
+const PLUGINS = [
+    contenttypefixture_1.default,
+    projectgutenberg_1.default,
+    standardebooks_1.default,
+    aozorabunko_1.default,
+    ndlnextdigitallibrary_1.default,
+    githubdocs_1.default,
+    komga_1.default,
+    oapen_1.default,
+    peppercarrot_1.default,
+];
 exports.default = PLUGINS;
